f7060660adee9884a591531ceb1e8c6d04b6436d
Александр Лебедев <all940722@gmail.com>
2019-10-27 00:28:37 +0300

    feature: addssection test

--- src/js/components/swiper-slider.js
+++ src/js/components/swiper-slider.js
@@ -1,15 +1,28 @@
-$(document).ready(function () {
-	var swiperMyths = new Swiper('.myths__container', {
-		loop: true,
-		spaceBetween: 100,
-		touchRatio: 0,
-		navigation: {
-			nextEl: '.swiper-next',
-		},
-		breakpoints: {
-			1200: {
-				autoHeight: true,
-			}
-		},
-	});
+var swiperMyths = new Swiper('.myths__container', {
+	loop: true,
+	spaceBetween: 100,
+	touchRatio: 0,
+	navigation: {
+		nextEl: '.swiper-next',
+	},
+	breakpoints: {
+		1200: {
+			autoHeight: true,
+		}
+	},
+});
+
+var swiperTest = new Swiper('.test__container', {
+	spaceBetween: 100,
+	touchRatio: 0,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	navigation: {
+		nextEl: '.swiper-next',
+	},
+	effect: 'fade',
+	fadeEffect: {
+		crossFade: true
+	},
 });
