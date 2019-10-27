61d8909bcc67664a01ac89f2df3df3412748feb9
Александр Лебедев <all940722@gmail.com>
2019-10-27 18:19:35 +0300

    feature: adds mobile version test and result

--- src/js/components/swiper-slider.js
+++ src/js/components/swiper-slider.js
@@ -1,28 +1,35 @@
-var swiperMyths = new Swiper('.myths__container', {
-	loop: true,
-	spaceBetween: 100,
-	touchRatio: 0,
-	navigation: {
-		nextEl: '.swiper-next',
-	},
-	breakpoints: {
-		1200: {
-			autoHeight: true,
-		}
-	},
-});
+$(document).ready(function () {
+	var swiperMyths = new Swiper('.myths__container', {
+		loop: true,
+		spaceBetween: 100,
+		touchRatio: 0,
+		navigation: {
+			nextEl: '.swiper-next',
+		},
+		breakpoints: {
+			1200: {
+				autoHeight: true,
+			}
+		},
+	});
 
-var swiperTest = new Swiper('.test__container', {
-	spaceBetween: 100,
-	touchRatio: 0,
-	pagination: {
-		el: '.swiper-pagination',
-	},
-	navigation: {
-		nextEl: '.swiper-next',
-	},
-	effect: 'fade',
-	fadeEffect: {
-		crossFade: true
-	},
+	var swiperTest = new Swiper('.test__container', {
+		spaceBetween: 100,
+		touchRatio: 0,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		navigation: {
+			nextEl: '.swiper-next',
+		},
+		breakpoints: {
+			1200: {
+				autoHeight: true,
+			}
+		},
+		/* effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+		}, */
+	});
 });
